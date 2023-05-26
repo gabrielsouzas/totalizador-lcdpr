2bfef245444f90add7fdd3ca99f71fac318af87a
Gabriel Souza <104937852+gabrielsouzas@users.noreply.github.com>
2023-05-26 17:07:21 -0300

    Testes tabela

--- javascript/index.js
+++ javascript/index.js
@@ -1,4 +1,14 @@
+var Q200 = [
+    {
+        "indice": "Q200",
+        "data": "01010001",
+        "entrada": "000",
+        "saida": "000",
+        "saldo": "000",
+    }
+]
 
+const table_q200 = document.querySelector('.table-q200')
 
 function handleFile(files){
     const reader = new FileReader();
@@ -12,9 +22,19 @@ function handleFile(files){
         lines.forEach(line => {
             let arrayLine = line.split("|");
             if (arrayLine[0] == "Q200") {
-                
+                let novo_q200 = {
+                    "indice": arrayLine[0],
+                    "data": arrayLine[1],
+                    "entrada": arrayLine[2],
+                    "saida": arrayLine[3],
+                    "saldo": arrayLine[4],
+                }
+                Q200.push(novo_q200);
             }
         });
+
+        Q200.shift();
+        console.log(Q200);
     };
 };
 
