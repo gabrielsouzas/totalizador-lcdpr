4af72763e716b23a844980597179f7720e11802c
Gabriel Souza <104937852+gabrielsouzas@users.noreply.github.com>
2023-05-29 17:50:17 -0300

    Ajustes abas e lógica tabela

--- javascript/index.js
+++ javascript/index.js
@@ -8,9 +8,30 @@ var Q200 = [
     }
 ]
 
-const table_q200 = document.querySelector('.table-q200')
+//const table_q200 = document.querySelector('.table-q200')
+const table_q200 = document.createElement('table');
+table_q200.classList.add('table-q200');
+const q200_headers = [
+    "Código",
+    "Data",
+    "Entrada",
+    "Saída",
+    "Saldo",
+];
+
+function createHeadersTable(table, headers) {
+    //table.innerHTML = '<table><tr>';
+    const tr = document.createElement('tr');
+    headers.forEach(header => {
+        let th = document.createElement('th');
+        th.innerHTML = header;
+        tr.appendChild(th);
+    });
+    table.appendChild(tr);
+}
 
 function handleFile(files){
+    createHeadersTable(table_q200, q200_headers);
     const reader = new FileReader();
     // Lê o arquivo
     reader.readAsText(files[0]);
@@ -41,7 +62,9 @@ function handleFile(files){
         });
 
         Q200.shift();
-        console.log(Q200);
+        conteudos[3].innerHTML = "";
+        conteudos[3].appendChild(table_q200);
+        //console.log(Q200);
     };
 };
 
@@ -59,6 +82,8 @@ function formatMoney(value) {
 
 const abas = document.querySelectorAll('.aba');
 const conteudos = document.querySelectorAll('.conteudo');
+
+conteudos[0].style.display = 'flex';
 
 abas.forEach(aba => {
     aba.addEventListener('click', () => {
