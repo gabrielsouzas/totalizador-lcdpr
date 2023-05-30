77fa7af70c6b3a35f389d7ad7d68badc79f49130
Gabriel Souza <104937852+gabrielsouzas@users.noreply.github.com>
2023-05-30 17:03:13 -0300

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -82,6 +82,9 @@ function createHeadersTable(table, headers) {
 
 /* FUNÇÂO PRINCIPAL DO TRATAMENTO DO ARQUIVO */
 function handleFile(files){
+    createHeadersTable(table_0030, headers_0030);
+    createHeadersTable(table_0040, headers_0040);
+    createHeadersTable(table_q100, headers_q100);
     createHeadersTable(table_q200, q200_headers);
     const reader = new FileReader();
     // Lê o arquivo
@@ -93,22 +96,73 @@ function handleFile(files){
 
         lines.forEach(line => {
             let arrayLine = line.split("|");
-            preencherTabelas(table_q200, arrayLine);
+            preencherTabelas(arrayLine);
             
         });
 
         //Q200.shift();
-        conteudos[3].innerHTML = "";
+        /*conteudos[3].innerHTML = "";
         conteudos[3].appendChild(table_q200);
-        conteudos[3].style.alignItems = 'flex-start';
+        conteudos[3].style.alignItems = 'flex-start';*/
         //console.log(Q200);
+
+        configAbaContent(conteudos);
     };
 };
 
 /* PREENCHIMENTO DAS TABELAS DE ACORDO COM O TIPO DE REGISTRO */
-function preencherTabelas(tabela, dados) {
-    if (dados[0] == "Q200") {
-        tabela.innerHTML += `<tr>
+function preencherTabelas(dados) {
+    if (dados[0] == "0030") {
+        table_0030.innerHTML += `<tr>
+                                    <td>${dados[0]}</td>
+                                    <td>${dados[1]}</td>
+                                    <td>${dados[2]}</td>
+                                    <td>${dados[3]}</td>
+                                    <td>${dados[4]}</td>
+                                    <td>${dados[5]}</td>
+                                    <td>${dados[6]}</td>
+                                    <td>${dados[7]}</td>
+                                    <td>${dados[8]}</td>
+                                    <td>${dados[9]}</td>
+                                </tr>`;
+    } else if (dados[0] == "0040") {
+        table_0040.innerHTML += `<tr>
+                                    <td>${dados[0]}</td>
+                                    <td>${dados[1]}</td>
+                                    <td>${dados[2]}</td>
+                                    <td>${dados[3]}</td>
+                                    <td>${dados[4]}</td>
+                                    <td>${dados[5]}</td>
+                                    <td>${dados[6]}</td>
+                                    <td>${dados[7]}</td>
+                                    <td>${dados[8]}</td>
+                                    <td>${dados[9]}</td>
+                                    <td>${dados[10]}</td>
+                                    <td>${dados[11]}</td>
+                                    <td>${dados[12]}</td>
+                                    <td>${dados[13]}</td>
+                                    <td>${dados[14]}</td>
+                                    <td>${dados[15]}</td>
+                                    <td>${dados[16]}</td>
+                                </tr>`;
+    } else if (dados[0] == "Q100") {
+        table_q100.innerHTML += `<tr>
+                                    <td>${dados[0]}</td>
+                                    <td>${formatDate(dados[1])}</td>
+                                    <td>${dados[2]}</td>
+                                    <td>${dados[3]}</td>
+                                    <td>${dados[4]}</td>
+                                    <td>${dados[5]}</td>
+                                    <td>${dados[6]}</td>
+                                    <td>${dados[7]}</td>
+                                    <td>${dados[8]}</td>
+                                    <td>${formatMoney(dados[9])}</td>
+                                    <td>${formatMoney(dados[10])}</td>
+                                    <td>${formatMoney(dados[11])}</td>
+                                    <td>${dados[12]}</td>
+                                </tr>`;
+    } else if (dados[0] == "Q200") {
+        table_q200.innerHTML += `<tr>
                                     <td>${dados[0]}</td>
                                     <td>${formatDate(dados[1])}</td>
                                     <td>${formatMoney(dados[2])}</td>
@@ -116,6 +170,19 @@ function preencherTabelas(tabela, dados) {
                                     <td>${formatMoney(dados[4])}</td>
                                 </tr>`;
     }
+}
+
+function configAbaContent(abaContents) {
+    abaContents.forEach(abaContent => {
+        abaContent.innerHTML = "";
+        abaContent.style.alignItems = 'flex-start';
+        abaContent.style.justifyContent = 'flex-start';
+    });
+    
+    abaContents[0].appendChild(table_0030);
+    abaContents[1].appendChild(table_0040);
+    abaContents[2].appendChild(table_q100);
+    abaContents[3].appendChild(table_q200);
 }
 
 function formatDate(value) {
